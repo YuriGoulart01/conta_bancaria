599f328d5a209f07b906947c92f38ea70942145b
Yuri <yurigoulart8@gmail.com>
2025-11-06 15:19:18 -0300

    Menu implementado

---
+++ Menu.ts
@@ -0,0 +1,14 @@
+console.log("*****************************************************");
+console.log("           BANCO DO BRAZIL COM Z");
+console.log("*****************************************************");
+console.log("\n         1- Criar conta");
+console.log("         2- Listar todas as contas");
+console.log("         3- Buscar conta por número");
+console.log("         4- Atualizar dados da conta");
+console.log("         5- Apagar conta");
+console.log("         6- Sacar");
+console.log("         7- Depositar");
+console.log("         8- Transferir valores entre contas");
+console.log("         9- Sair");
+console.log("\n*****************************************************");
+console.log("\nDigite a opção desejada: ")
